0f48fd9c66a00197df948601ec8b48890975502a
Rick000000007 <rickmajumdar2019@gmail.com>
2026-03-05 13:55:14 +0530

    Simplify MainActivity to fix build

--- app/src/main/java/com/goatx/MainActivity.kt
+++ app/src/main/java/com/goatx/MainActivity.kt
@@ -1,35 +1,18 @@
 package com.goatx
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.goatx.nativebridge.NativeLoader
-import com.goatx.nativebridge.NativePty
-import com.goatx.terminal.TerminalView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var terminalView: TerminalView
-    private lateinit var nativePty: NativePty
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Load native library
-        NativeLoader.load()
+        val textView = TextView(this)
+        textView.text = "GOAT-1 Terminal App"
+        textView.textSize = 20f
 
-        // Create terminal view
-        terminalView = TerminalView(this)
-        setContentView(terminalView)
-
-        // Create PTY without lambda constructor
-        nativePty = NativePty()
-
-        // Start shell
-        nativePty.startShell("/system/bin/sh")
-    }
-
-    override fun onDestroy() {
-        nativePty.destroy()
-        super.onDestroy()
+        setContentView(textView)
     }
 }
